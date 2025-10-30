d47184a69cb70cc1ecda71b0b4a11ba5236920e5
mitesh-bhoite <mitesh07bhoite@gmail.com>
2025-10-30 07:59:09 +0530

    other commit - DevSync MERN app

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://devsync-6hf6.onrender.com"],
     credentials: true,
   })
 );
